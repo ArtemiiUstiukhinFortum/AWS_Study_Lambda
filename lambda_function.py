3468b862a2aa764c097c9cc03337b67753608f2a
Ville Väänänen <ville.vaananen@fortum.com>
2022-06-30 23:08:06 +0300

    Return the HTML page from the Lambda

--- lambda_function.py
+++ lambda_function.py
@@ -1,12 +1,19 @@
 import json
 from base64 import b64decode
-from typing import Dict
 from urllib.parse import parse_qs
 
 import boto3
 import botocore
-from aws_lambda_powertools import Logger
-from aws_lambda_powertools.utilities.typing import LambdaContext
+from aws_lambda_powertools import Logger, Metrics, Tracer
+from aws_lambda_powertools.event_handler.api_gateway import APIGatewayHttpResolver
+from aws_lambda_powertools.logging import correlation_paths
+
+service = "COFFEE_APP"
+logger = Logger(service=service)
+tracer = Tracer(service=service)
+metrics = Metrics(namespace="MyApp", service=service)
+app = APIGatewayHttpResolver()
+
 
 sqs = boto3.client("sqs")
 sns = boto3.client("sns")
@@ -17,15 +24,23 @@ sns_topic_arn_base = "arn:aws:sns:eu-west-1:712531873081:drink-order-topic-for-"
 logger = Logger()
 
 
-@logger.inject_lambda_context(log_event=True)
-def lambda_handler(event: Dict, context: LambdaContext):
+@app.get("/")
+@tracer.capture_method
+def index():
+    with open("index.html") as f:
+        return {"message": f.read()}
 
-    if "body" not in event or not event["body"]:
-        return {
-            "statusCode": 200,
-            "body": "Please submit an order",
-        }
 
+@app.get("/hello")
+@tracer.capture_method
+def hello():
+    return {"message": "Hello world!"}
+
+
+@app.post("/")
+@tracer.capture_method
+def submit_order():
+    event = app.current_event
     body = (
         b64decode(event["body"]).decode("utf-8")
         if event["isBase64Encoded"]
@@ -51,7 +66,7 @@ def lambda_handler(event: Dict, context: LambdaContext):
                 ),
             }
     except botocore.exceptions.ClientError as e:
-        logger.error(e)
+        logger.exception(e)
         # You need to create the topic and subsribe the email here
         return {
             "statusCode": 200,
@@ -62,9 +77,7 @@ def lambda_handler(event: Dict, context: LambdaContext):
 
     messageToSQS = {"name": name, "email": email, "drink": drink}
 
-    sqsresponce = sqs.send_message(
-        QueueUrl=queue_url, MessageBody=json.dumps(messageToSQS)
-    )
+    sqs.send_message(QueueUrl=queue_url, MessageBody=json.dumps(messageToSQS))
 
     return {
         "statusCode": 200,
@@ -73,3 +86,16 @@ def lambda_handler(event: Dict, context: LambdaContext):
             your drink is ready. Your order: "{drink}" for "{name}"
         """,
     }
+
+
+@tracer.capture_lambda_handler
+@logger.inject_lambda_context(
+    correlation_id_path=correlation_paths.API_GATEWAY_HTTP, log_event=True
+)
+@metrics.log_metrics(capture_cold_start_metric=True)
+def lambda_handler(event, context):
+    try:
+        return app.resolve(event, context)
+    except Exception as e:
+        logger.exception(e)
+        raise
